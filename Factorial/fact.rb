4551d944e11b895e1af8eac505c934a085ff14d1
sachin1092 <me@sachinshinde.com>
2014-12-31 12:22:23 +0530

    Factorial: Ruby Benchmarking inject

--- Factorial/fact.rb
+++ Factorial/fact.rb
@@ -27,7 +27,7 @@ puts "Enter number: "
 num = gets.chomp.to_i
 bench1 = Benchmark.measure { puts ft_recursive(num) } 
 bench2 = Benchmark.measure { puts ft_loop(num) } #7.567 + 7.499 + 7.45 + 7.698 ~ 7.5535
-bench3 = Benchmark.measure { puts ft_inject(num) } 
+bench3 = Benchmark.measure { puts ft_inject(num) } #7.542 + 7.639 + 7.482 + 7.412 ~ 7.51875
 puts "Completed in #{bench1}"
 puts "Completed in #{bench2}"
 puts "Completed in #{bench3}"
