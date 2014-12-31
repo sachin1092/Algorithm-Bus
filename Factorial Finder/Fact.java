ae50c0bb22975f3b20b62e4b75c26cea764c4cf9
sachin1092 <me@sachinshinde.com>
2014-12-31 23:18:07 +0530

    Factorial Java: print added

--- Factorial Finder/Fact.java
+++ Factorial Finder/Fact.java
@@ -2,6 +2,13 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Fact {
+
+	/**
+	 * [ftRecursiveTail Factorial using tail recursion]
+	 * @param  acc [accumulator]
+	 * @param  n   [number to find factorial for]
+	 * @return Factorial of n [description]
+	 */
 	public static BigInteger ftRecursiveTail(BigInteger acc, BigInteger n) {
 		if(n == BigInteger.ZERO)
 			return acc; 
@@ -16,6 +23,18 @@ public class Fact {
 	 	Scanner s = new Scanner(System.in);
 	    System.out.print("Enter a number: ");
 	    int n = s.nextInt();
-	    System.out.println("Factorial is: " + ftHelper(n));
+	    long t1 = System.currentTimeMillis();
+	    print("Factorial is:", ftHelper(n)); //~9918
+	    long t2 = System.currentTimeMillis();
+	    float dt = (t2 - t1);
+	    dt = (float)((float)dt/(float)1000);
+	    print("Tail Recursion in:", dt);
+	}
+
+	public static void print(Object... msg) {
+		String new_msg = "";
+        for(Object m : msg)
+            new_msg = new_msg + " " + m.toString();
+		System.out.println(new_msg);
 	}
 }
