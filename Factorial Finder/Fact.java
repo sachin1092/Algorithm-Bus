562c4e7e7172e17fdcf20f9a46f4cb504b5134d2
sachin1092 <me@sachinshinde.com>
2014-12-31 23:56:52 +0530

    Factorial Java: Iterative done

--- Factorial Finder/Fact.java
+++ Factorial Finder/Fact.java
@@ -21,7 +21,7 @@ public class Fact {
 	}
 
 	/**
-	 * [ftRecursive Factorial using tail recursion]
+	 * [ftRecursive Factorial using recursion]
 	 * @param  n   [number to find factorial for]
 	 * @return Factorial of n
 	 */
@@ -36,26 +36,50 @@ public class Fact {
 		return ftRecursive(new BigInteger(String.valueOf(n)));
 	}	
 
+
+	/**
+	 * [ftLoop Factorial]
+	 * @param  n   [number to find factorial for]
+	 * @return Factorial of n
+	 */
+	public static BigInteger ftLoop(int n) {
+		BigInteger x = BigInteger.ONE;
+		for(int i = 1 ; i <= n ; i++) {
+			x = x.multiply(new BigInteger(String.valueOf(i)));
+		}
+		return x;		
+	}
+
 	public static void main(String[] args) {
 	 	Scanner s = new Scanner(System.in);
 	    System.out.print("Enter a number: ");
 	    int n = s.nextInt();
+	    long t1, t2;
+	    float dt;
 
 	    //Tail Recursion
-	    // long t1 = System.currentTimeMillis();
+	    // t1 = System.currentTimeMillis();
 	    // print("Factorial is:", ftHelper(n)); //~9918
-	    // long t2 = System.currentTimeMillis();
-	    // float dt = (t2 - t1); //0.477
-	    // dt = (float)((float)dt/(float)1000);
+	    // t2 = System.currentTimeMillis();
+	    // dt = (t2 - t1); //0.477
+	    // dt = (float)(dt/1000f);
 	    // print("Tail Recursion in:", dt);
 
 	    //Recursion
-	    long t1 = System.currentTimeMillis();
-	    print("Factorial is:", ftRecursive(n)); //~9154
-	    long t2 = System.currentTimeMillis();
-	    float dt = (t2 - t1); //0.323
-	    dt = (float)((float)dt/(float)1000);
-	    print("Recursion in:", dt);
+	    // t1 = System.currentTimeMillis();
+	    // print("Factorial is:", ftRecursive(n)); //~9154
+	    // t2 = System.currentTimeMillis();
+	    // dt = (t2 - t1); //0.308
+	    // dt = (float)(dt/1000f);
+	    // print("Recursion in:", dt);
+
+	    //Iterative
+	    t1 = System.currentTimeMillis();
+	    print("Factorial is:", ftLoop(n)); //~100000 //~9154 //~9918
+	    t2 = System.currentTimeMillis();
+	    dt = (t2 - t1); //~48.612 // //
+	    dt = (float)(dt/1000f);
+	    print("Iterative in:", dt);
 	}
 
 	public static void print(Object... msg) {
