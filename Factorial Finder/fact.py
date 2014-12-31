a399edd271b65a07f848b1f31123f057f6ce4a73
sachin1092 <me@sachinshinde.com>
2014-12-31 16:22:38 +0530

    Factorial Python: functional implemented

--- Factorial Finder/fact.py
+++ Factorial Finder/fact.py
@@ -9,6 +9,12 @@ def ft_loop(n):
         result *= i
     return result
 
+from operator import mul
+from functools import reduce
+ 
+def ft_functional(n):
+    return reduce(mul, range(1,n+1), 1)
+
 def ft_recursive(n):
     z=1
     if n>1:
@@ -31,9 +37,11 @@ print ">"*5, "Factorial", "<"*5
 print "Enter number: "
 n = int(raw_input("Enter a number: "))
 b1 = ft_print(ft_loop, n)
-b2 = ft_print(ft_recursive, n) #~ 21825
-b3 = ft_print(ft_tail_recursive, n) #~ 21825
+b2 = ft_print(ft_functional, n)
+b3 = ft_print(ft_recursive, n) #~ 21825
+b4 = ft_print(ft_tail_recursive, n) #~ 21825
 
 print b1
 print b2
 print b3
+print b4
