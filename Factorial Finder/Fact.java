69fb9ca6c0d4883171350bb4fdf4f6892266616e
sachin1092 <me@sachinshinde.com>
2014-12-31 23:45:13 +0530

    Factorial Java: Recursion implemented

--- Factorial Finder/Fact.java
+++ Factorial Finder/Fact.java
@@ -1,5 +1,6 @@
 import java.math.BigInteger;
 import java.util.Scanner;
+import java.lang.reflect.Method;
 
 public class Fact {
 
@@ -19,16 +20,42 @@ public class Fact {
 		return ftRecursiveTail(new BigInteger("1"), new BigInteger(String.valueOf(n)));
 	}
 
+	/**
+	 * [ftRecursive Factorial using tail recursion]
+	 * @param  n   [number to find factorial for]
+	 * @return Factorial of n
+	 */
+	public static BigInteger ftRecursive(BigInteger n) {
+		BigInteger x = BigInteger.ONE;
+		if(n.compareTo(BigInteger.ONE) == 1)
+			x = n.multiply(ftRecursive(n.subtract(BigInteger.ONE)));
+		return x;
+	}
+
+	public static BigInteger ftRecursive(int n) {
+		return ftRecursive(new BigInteger(String.valueOf(n)));
+	}	
+
 	public static void main(String[] args) {
 	 	Scanner s = new Scanner(System.in);
 	    System.out.print("Enter a number: ");
 	    int n = s.nextInt();
+
+	    //Tail Recursion
+	    // long t1 = System.currentTimeMillis();
+	    // print("Factorial is:", ftHelper(n)); //~9918
+	    // long t2 = System.currentTimeMillis();
+	    // float dt = (t2 - t1); //0.477
+	    // dt = (float)((float)dt/(float)1000);
+	    // print("Tail Recursion in:", dt);
+
+	    //Recursion
 	    long t1 = System.currentTimeMillis();
-	    print("Factorial is:", ftHelper(n)); //~9918
+	    print("Factorial is:", ftRecursive(n)); //~9154
 	    long t2 = System.currentTimeMillis();
-	    float dt = (t2 - t1);
+	    float dt = (t2 - t1); //0.323
 	    dt = (float)((float)dt/(float)1000);
-	    print("Tail Recursion in:", dt);
+	    print("Recursion in:", dt);
 	}
 
 	public static void print(Object... msg) {
