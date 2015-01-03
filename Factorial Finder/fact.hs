98b354cb5f72acbdf2df676203aa6698f59ace87
sachin1092 <me@sachinshinde.com>
2015-01-03 10:17:55 +0530

    Factorial haskell: simple product method

--- Factorial Finder/fact.hs
+++ Factorial Finder/fact.hs
@@ -1,6 +1,9 @@
---Todo
-fac n
+--Tail Recursive
+fact_rec n
     | n >= 0    = go 1 n
     | otherwise = error "Negative factorial!"
         where go acc 0 = acc
               go acc n = go (acc * n) (n - 1)
+
+--Simple product method
+fact_prod n = product [1..n]
