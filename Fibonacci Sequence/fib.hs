6142e877c3b78c9ece2f7e4ceab4978a6147e87b
sachin1092 <me@sachinshinde.com>
2015-01-23 10:12:21 +0530

    Fib Haskel: another approach

--- Fibonacci Sequence/fib.hs
+++ Fibonacci Sequence/fib.hs
@@ -5,3 +5,5 @@ let fib x =  if x < 1 then 0 else (if x < 3 then 1 else (fib(x - 1) + fib(x - 2)
 --This is a standard example how to use lazy lists. Here's the (infinite) list of all Fibonacci numbers:
 fib = 0 : 1 : zipWith (+) fib (tail fib)
 
+--The nth Fibonacci number is then just fib !! n.
+fib = 0 : 1 : next fib where next (a: t@(b:_)) = (a+b) : next t
