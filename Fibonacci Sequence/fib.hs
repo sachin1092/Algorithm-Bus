73ba927bf5c9523cadb740d5179c74167a2d9d71
sachin1092 <me@sachinshinde.com>
2015-01-23 10:11:14 +0530

    Fib Haskel: lazy loading

--- Fibonacci Sequence/fib.hs
+++ Fibonacci Sequence/fib.hs
@@ -1,3 +1,7 @@
 --functional; recursive; one-line;
 
 let fib x =  if x < 1 then 0 else (if x < 3 then 1 else (fib(x - 1) + fib(x - 2)))
+
+--This is a standard example how to use lazy lists. Here's the (infinite) list of all Fibonacci numbers:
+fib = 0 : 1 : zipWith (+) fib (tail fib)
+
